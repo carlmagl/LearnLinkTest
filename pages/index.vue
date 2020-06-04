52c226194cfeb6beefa87c4310b432db3621651c
Carl-Magnus Lein <carl@live.no>
2020-06-04 23:36:57 +0200

    trying to launch website

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,18 @@
   <div>
     <div class="container">
       <div>
-        <h1 class="title">Carls plant shit</h1>
-        <h2 class="subtitle">Find my plants</h2>
-        <ul id="example-1">
-          <li v-for="country in getCountries" :key="country.name">{{ country.name }}</li>
-          <div>Noe her</div>
+        <h1 class="title">Find a new country</h1>
+        <h2 class="subtitle">See every country located on earth</h2>
+        <ul class="list-group">
+          <li class="list-group-item country" v-for="country in getCountries" :key="country.name">
+            <div class="titlecard">{{ country.name }}</div>
+            <div class="subtitlecard">
+              {{country.name}}'s capital is
+              {{ country.capital }}.
+            </div>
+            <div class="subtitlecard">The population in {{country.name}} is {{ country.population }} people.</div>
+            <div class="subtitlecard">It's located in {{ country.subregion }}.</div>
+          </li>
         </ul>
       </div>
     </div>
@@ -16,40 +23,17 @@
 <script>
 import Logo from "~/components/Logo.vue";
 import Axios from "axios";
-import { mapGetters } from 'vuex'
-
+import { mapGetters } from "vuex";
 
 let state = [];
 const endpoint = "https://restcountries-v1.p.rapidapi.com/all";
 export default {
-   computed: {
-    ...mapGetters([
-      'getCountries',
-    ])
+  computed: {
+    ...mapGetters(["getCountries"])
   },
   components: {
     Logo
-  },
- /*  beforeMount: () => {
-    var getData = async url => {
-      try {
-        let data = await Axios.get(url, {
-          headers: {
-            "x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
-            "x-rapidapi-key":
-              "b6d30ad204mshda48b7cd0cc5f9ep143885jsn188cc3b94a8c",
-            useQueryString: true
-          }
-        });
-        return data;
-      } catch (e) {
-        console.log(e);
-      }
-    };
-
-    state = getData(endpoint);
-    console.log(state);
-  } */
+  }
 };
 </script>
 
@@ -63,7 +47,7 @@ export default {
   text-align: center;
 }
 
-.title {
+.titlecard {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
     "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
@@ -73,13 +57,33 @@ export default {
   letter-spacing: 1px;
 }
 
-.subtitle {
+.subtitlecard {
   font-weight: 300;
   font-size: 42px;
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
 }
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: white;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: white;
+  letter-spacing: 1px;
+}
+
+
 
 .links {
   padding-top: 15px;
@@ -89,5 +93,16 @@ ul {
   list-style-type: none;
   margin: 0;
   padding: 0;
+}
+
+.country {
+  padding: 40px;
+  margin: 40px;
+}
+
+.wrapper {
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
 }
 </style>
